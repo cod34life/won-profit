80c82792ffb01dd320a7dff8f041ccedd8563780
haastrup elijah <haastrupea@gmail.com>
2020-08-23 16:51:39 +0100

    Refactor: redesign landing page
    the landing page is now responsive
    
    it is now more visually appealing

--- index.php
+++ index.php
@@ -6,15 +6,20 @@
 </head>
 <body>
      <header>
-     	<div class="top">
-     	     <h1>Welcome to Won-Profit</h1>
+     <div class="header-page-content">
+		<div class="hader-page-inner">
+		<div class="top">
+     	     <h1>Welcome to 
+							<br><span> Won-Profit</span></h1>
         </div>
          <div class="tagline">
-         	<h2>The nonprofits receives your donation and you get the chance to donate to causes that you care about</h2>
+         	<p>The nonprofits receives your donation and you get the chance to donate to causes that you care about</p>
          	<div class="Donate">
          		<a href="interest.php" class="bttn" >Donate now</a>
             </div>
-         </div>   
+         </div> 
+		</div> 
+		 </div> 
      </header>
 </body>
 </html>
