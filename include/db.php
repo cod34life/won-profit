4e8475308020c94393cef3439a453e86234f77de
Sumeet Naik <sumeetnaikcs@gmail.com>
2020-08-23 20:22:02 +0530

    Update db.php

--- include/db.php
+++ include/db.php
@@ -1,6 +1,6 @@
 <?php
 // Database Credentials
-define('DBHOST','localhost'); //shareddb-m.hosting.stackcp.net
+define('DBHOST','localhost');
 define('DBUSER','wonprofit-313031f02b');
 define('DBPASS','19dn0hw6ze');
 define('DBNAME','wonprofit-313031f02b');
